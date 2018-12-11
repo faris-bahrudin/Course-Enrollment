dcac950722876f427845427f5709a5d4859f2911
hafidz zakaria <hafidz.zakaria@terang.asia>
2018-12-11 18:12:28 +0800

    student list page html update

--- src/app/course-enrollment/course-list.page.ts
+++ src/app/course-enrollment/course-list.page.ts
@@ -4,7 +4,7 @@ import {Observable} from 'rxjs';
 import {CourseEnrollmentService} from '../../services/course-enrollment.service';
 
 @Component({
-  selector: 'trg-apprentice-list',
+  selector: 'trg-course-list',
   templateUrl: './course-list.page.html',
 })
 export class CourseListPage {
