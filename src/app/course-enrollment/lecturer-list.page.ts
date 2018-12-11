88d3ba7bae9f89b61acf37d24e3a58c14f1ec0d4
Faris Bahrudin <faris.bahrudin@terang.asia>
2018-12-11 17:37:05 +0800

    Lecturer page

--- src/app/course-enrollment/lecturer-list.page.ts
+++ src/app/course-enrollment/lecturer-list.page.ts
@@ -1,14 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Lecturer} from './lecturer.model';
+import {Observable} from 'rxjs';
+import {CourseEnrollmentService} from '../../services/course-enrollment.service';
 
 
 @Component({
-  selector: 'trg-apprentice-list',
-  templateUrl: './apprentice-list.page.html',
+  selector: 'trg-lecturer-list',
+  templateUrl: './lecturer-list.page.html',
 })
-export class LecturerListPage {
+export class LecturerListPage implements OnInit {
 
+  public columns = ['id', 'lecturername', 'staffnumber', 'email'];
+  public dataSource: Observable<Lecturer[]>;
 
-  constructor() {
+  constructor(private courseEnrollmentService: CourseEnrollmentService) {
   }
 
+  ngOnInit() {
+    this.dataSource = this.courseEnrollmentService.findLecturers();
+  }
+
+  view(lecturer: Lecturer): void {
+    console.log(JSON.stringify(lecturer));
+    // this.router.navigate(['/academy/students/', cohort.code]);
+  }
 }
